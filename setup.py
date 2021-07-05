9ae864c8622d39e742d7e8cec721521908fb0465
zhengxingXue <zx252cornell@gmail.com>
2021-07-05 11:17:03 -0500

    add pygame_widgets

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(name='Gomoku',
       version='0.0.0',
-      install_requires=['gym', 'numpy', 'matplotlib', 'pytest',
-                        'stable-baselines3', 'coverage', 'pygame']
+      install_requires=['gym', 'numpy', 'matplotlib',
+                        'pytest', 'coverage',
+                        'stable-baselines3',
+                        'pygame', 'pygame_widgets']
 )
